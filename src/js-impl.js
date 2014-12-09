15fc7b7d60a91b311275b121a4d4148da6bf21e9
Andy Hanson <andy.pj.hanson@gmail.com>
2014-12-08 22:14:49 -0800

    More js literal removal

--- src/js-impl.js
+++ src/js-impl.js
@@ -1,10 +1,9 @@
 "use strict"
 
+const ms = require("./private/bootstrap").ms
+
 module.exports = {
-	"each-generator": function*(iter, doEach) {
-		for (let em of iter)
-			yield* doEach(em)
-	},
+	// js.ms
 	"i!": function(a) { return !a },
 	"i~": function(a) { return ~a },
 	"i-bar": function(a, b) { return a | b },
@@ -13,9 +12,53 @@ module.exports = {
 	"i-new": function(constructor) {
 		return new (Function.prototype.bind.apply(constructor, arguments))
 	},
+	"i-oh-no!": function(error) {
+		throw module.exports["make-Error"](error)
+	},
 	"i-sub": function(a, b) { return a[b] },
 	"i-set": function(a, b, c) { a[b] = c },
 	"i-true": true,
+	"i-typeof": function(a) { return typeof a },
+
+	// Generator.ms
+	"each-generator": function*(iter, doEach) {
+		for (let em of iter)
+			yield* doEach(em)
+	},
+
+	// Try.ms
+	"i-always-do-after": function(tried, finallyDo) {
+		try {
+			return tried()
+		}
+		finally {
+			finallyDo()
+		}
+	},
+	"i-try": function(makeSuccess, tried) {
+		try {
+			return makeSuccess(tried())
+		}
+		catch (e) {
+			return e
+		}
+	},
+	"make-Error": function(error) {
+		let err
+		try {
+			err = ms.unlazy(error)
+		}
+		catch (e) {
+			return new Error ("Error making error: " + e.message)
+		}
+		if (err instanceof Error)
+			return err
+		if (typeof err === "string")
+			return new Error(err)
+		if (err == null)
+			return new Error("Oh no!")
+		return new Error("Argument to `oh-no!` must be Error or String")
+	}
 }
 
 const binOps = [ "&", "^", "<<", ">>", ">>>", "===", "<", ">", "<=", ">=", "+", "-", "*", "/", "%" ]
